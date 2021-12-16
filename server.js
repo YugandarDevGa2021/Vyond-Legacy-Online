8f36227c01545da35b52b4be577c0d2e1b99a1d8
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-12-16 00:02:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const asu = require('./asset/upload');
 const stl = require('./static/load');
 const stp = require('./static/page');
 const sto = require('./static/pageStudio');
+const plo = require('./static/pagelvmold');
 const stc = require('./static/pagecc');
 const scc = require('./static/pageccbro');
 const slv = require('./static/pagelvp');
@@ -56,6 +57,7 @@ const functions = [
 	scc,
 	slv,
 	stl,
+	plo,
 ];
 
 module.exports = http.createServer((req, res) => {
