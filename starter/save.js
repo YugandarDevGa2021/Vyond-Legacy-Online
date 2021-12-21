3d060ff9fdebbbed0b5b80e0ccf1cd23ed2677a7
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-12-20 20:05:09 -0500

    Update save.js

--- starter/save.js
+++ starter/save.js
@@ -11,7 +11,7 @@ module.exports = function (req, res, url) {
 
 		var body = Buffer.from(data.body_zip, 'base64');
 		var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, 'base64');
-		movie.save(body, thumb, data.movieId || data.presaveId, data.presaveId).then(nId => res.end('0' + nId));
+		starter.save(body, thumb, data.movieId || data.presaveId, data.presaveId).then(nId => res.end('0' + nId));
 	});
 	return true;
 }
